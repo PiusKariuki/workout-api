de385ef01cbc44341003161eaf4ce783f09cff6c
PiusKariuki <piuskariuki19@gmail.com>
2023-10-08 12:48:19 +0300

    New migration

--- DB/Models/movement_workout.py
+++ DB/Models/movement_workout.py
@@ -5,6 +5,7 @@ from DB.Models.base import TableBaseModel
 
 class MovementWorkoutJunction(TableBaseModel):
     id = Column(Integer, primary_key=True)
+    position = Column(Integer, nullable=False)
     movement_id = Column(Integer, ForeignKey("movement.id"), nullable=False)
     workout_id = Column(Integer, ForeignKey("workout.id"), nullable=False)
     sets = Column(Integer, nullable=False)
