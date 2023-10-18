10fb033d4c5819cb9d656de9208c6ac789f5ee60
PiusKariuki <piuskariuki19@gmail.com>
2023-10-18 16:24:27 +0300

    CORS middleware

--- app/main.py
+++ app/main.py
@@ -23,12 +23,15 @@ def start_application() -> FastAPI:
 
 app = start_application()
 
+
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
