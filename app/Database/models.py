b177979f30a7a2babade6d2cc495a5863442095d
PiusKariuki <piuskariuki19@gmail.com>
2023-10-26 11:27:03 +0300

    Removed unique date constraint for workouts since different accounts are now involved

--- app/Database/models.py
+++ app/Database/models.py
@@ -68,10 +68,6 @@ class Movement(SQLModel, table=True):
 
 
 class Workout(SQLModel, table=True):
-    __table_args__ = (
-        UniqueConstraint("date", name="unique_date_constraint"),
-    )
-
     id: Optional[int] = Field(primary_key=True, default=None)
     category_id: Optional[int] = Field(foreign_key="category.id", default=None)
     user_id: Optional[int] = Field(foreign_key="user.id", default=None)
